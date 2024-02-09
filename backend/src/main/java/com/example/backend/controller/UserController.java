00e77c525b4509f73dd56957f8c79f3ea3293ed1
kamalesh0617 <126144547+kamalesh1706@users.noreply.github.com>
2024-02-09 10:18:47 +0530

    Add files via upload

---
+++ backend/src/main/java/com/example/backend/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.example.backend.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+@RestController
+public class UserController {
+    @GetMapping("/")
+    public String index() {
+        return "hello Worlsdcsd";
+    }
+    
+}
